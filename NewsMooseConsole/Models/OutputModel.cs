3c97b263710357decc6430c66f7e335e78f48ba5
BotMaster3000 <jdgebauer@gmx.de>
2018-11-08 13:03:44 +0100

    Added Methods InitializeConsole, Added TestCode to Main-Method, Added Property

--- NewsMooseConsole/Models/OutputModel.cs
+++ NewsMooseConsole/Models/OutputModel.cs
@@ -11,5 +11,6 @@ namespace NewsMooseConsole.Models
         public int YPosStart { get; set; }
         public int XPosStart { get; set; }
         public int Length { get; set; }
+        public string Text { get; set; }
     }
 }
--- NewsMooseConsole/Program.cs
+++ NewsMooseConsole/Program.cs
@@ -10,7 +10,18 @@ namespace NewsMooseConsole
     {
         static void Main(string[] args)
         {
+            Controller.OutputController outputController = new Controller.OutputController(100, 40);
+            outputController.SetConsoleSize();
 
+            outputController.AddOutput(new Models.OutputModel()
+            {
+                XPosStart = 1,
+                YPosStart = 1,
+                Length = 7,
+                Text = "Hello, Test"
+            });
+
+            Console.ReadLine();
         }
     }
 }
