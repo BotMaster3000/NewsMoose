028252dc019d93c45f080883f05f571ff96084a4
BotMaster3000 <jdgebauer@gmx.de>
2018-12-20 11:34:29 +0100

    Creating Table if not exists

--- NewsMooseClassLibrary/DataBase/DatabaseStorage.cs
+++ NewsMooseClassLibrary/DataBase/DatabaseStorage.cs
@@ -25,15 +25,12 @@ namespace NewsMooseClassLibrary.DataBase
 
         private void InitializeDatabase()
         {
-            if (!File.Exists(DATA_SOURCE))
-            {
-                SQLiteCommand command = new SQLiteCommand(connection);
-                command.CommandText = "CREATE TABLE Publisher (publisher_id varchar(20), publishername varchar(50))";
-                command.ExecuteNonQuery();
+            SQLiteCommand command = new SQLiteCommand(connection);
+            command.CommandText = "CREATE TABLE IF NOT EXISTS Publisher (publisher_id varchar(20), publishername varchar(50))";
+            command.ExecuteNonQuery();
 
-                command.CommandText = "CREATE TABLE Newspaper (newspapername varchar(50), publisher_id varchar(20))";
-                command.ExecuteNonQuery();
-            }
+            command.CommandText = "CREATE TABLE IF NOT EXISTS Newspaper (newspapername varchar(50), publisher_id varchar(20))";
+            command.ExecuteNonQuery();
         }
 
         public void DeleteNewsLetters(List<NewsPaper> newsPaper)
@@ -56,14 +53,14 @@ namespace NewsMooseClassLibrary.DataBase
             Array publishers = this.LoadDataBase()?.Publishers;
             if (publishers != null)
             {
-               foreach(Publisher xpublisher in publishers)
+                foreach (Publisher xpublisher in publishers)
                 {
                     if (xpublisher.Name == publisher.Name)
                     {
                         return xpublisher.NewsPapers.ToList();
                     }
                 }
-                
+
             }
             return new List<NewsPaper>();
         }
@@ -88,10 +85,10 @@ namespace NewsMooseClassLibrary.DataBase
             SQLiteCommand command = new SQLiteCommand(connection);
             command.CommandText = "SELECT * FROM Publisher";
             command.ExecuteNonQuery();
-            SQLiteDataReader reader =  command.ExecuteReader();
+            SQLiteDataReader reader = command.ExecuteReader();
 
-            List <Publisher> publishers = new List<Publisher>();
-            foreach(Publisher xpublisher in reader)
+            List<Publisher> publishers = new List<Publisher>();
+            foreach (Publisher xpublisher in reader)
             {
                 publishers.Add(xpublisher);
             }
@@ -110,7 +107,7 @@ namespace NewsMooseClassLibrary.DataBase
             ClearDatabase();
 
             Dictionary<string, int> publisherNameAndId = new Dictionary<string, int>();
-            using (SQLiteCommand command = new SQLiteCommand())
+            using (SQLiteCommand command = new SQLiteCommand(connection))
             {
 
                 int counter = 0;
@@ -126,7 +123,13 @@ namespace NewsMooseClassLibrary.DataBase
 
                 foreach (NewsPaper paper in database.NewsPapers)
                 {
-                    command.CommandText = $"INSERT INTO Newspaper(newspapername, publisher_id) VALUES({paper.Name}, {publisherNameAndId[paper.Publisher.Name]})";
+                    string publisherID = "NULL";
+                    if (publisherNameAndId.Keys.Contains(paper.Publisher.Name))
+                    {
+                        publisherID = publisherNameAndId[paper.Publisher.Name].ToString();
+                    }
+
+                    command.CommandText = $"INSERT INTO Newspaper(newspapername, publisher_id) VALUES({paper.Name}, {publisherID})";
                     command.ExecuteNonQuery();
                 }
             }
@@ -134,7 +137,7 @@ namespace NewsMooseClassLibrary.DataBase
 
         private void ClearDatabase()
         {
-            using (SQLiteCommand command = new SQLiteCommand())
+            using (SQLiteCommand command = new SQLiteCommand(connection))
             {
                 command.CommandText = "DELETE FROM Publisher";
                 command.ExecuteNonQuery();
